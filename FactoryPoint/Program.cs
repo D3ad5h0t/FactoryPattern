d386e5e9636e100f30b7687ed0360d6815213d24
D3ad5h0t <alexgl007@mail.ru>
2020-06-10 11:37:45 +1000

    factory

---
+++ FactoryPoint/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FactoryPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}
---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Point.Factory.NewCartesianPoint(1, 3);
+        }
+    }
+    
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public static PointFactory Factory = new PointFactory();
+        
+        public class PointFactory
+        {
+            public Point NewCartesianPoint(double x, double y) => new Point(x, y);
+            
+            public Point NewPolarPoint(double rho, double theta) => new Point(
+                rho * Math.Cos(theta),
+                rho * Math.Sin(theta));
+        }
+    }
+}
